c8fd795d8c9768787877a24ef6dd46e00ecadbed
MUZAMMIL-003 <muhammadmuzzi90@gmail.com>
2025-07-13 22:36:52 +0500

    .

--- Modals/TaskModal.js
+++ Modals/TaskModal.js
@@ -1,14 +1,14 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const taskSchema = new mongoose.Schema({
-    taskheader: { type: String, required: true },
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    completed: { type: Boolean, default: false },
-}, {
-    timestamps: true,
-});
+// const taskSchema = new mongoose.Schema({
+//     taskheader: { type: String, required: true },
+//     title: { type: String, required: true },
+//     description: { type: String, required: true },
+//     completed: { type: Boolean, default: false },
+// }, {
+//     timestamps: true,
+// });
 
-const Task = mongoose.model("Task", taskSchema);
+// const Task = mongoose.model("Task", taskSchema);
 
-export default Task;
+// export default Task;
